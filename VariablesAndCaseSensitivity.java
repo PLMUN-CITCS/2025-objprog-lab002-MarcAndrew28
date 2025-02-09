84977019803364c3a3116b91659f57b71901caca
MARCBILLONES <billonesmarcandrew_bscs@plmun.edu.ph>
2025-02-09 20:44:35 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 01

---
+++ VariablesAndCaseSensitivity.java
@@ -0,0 +1,19 @@
+public class VariablesAndCaseSensitivity {
+    public static void main(String[] args) {
+
+String name = "Alice";
+int age = 18;
+double ip = 29.99;
+int count = 10;
+int count2 = 20;
+
+
+System.out.println("Student Age "+age);
+System.out.println("Item Price $"+ip);
+System.out.println("Student Name "+name);
+System.out.println("Counter "+count);
+System.out.println("Counter(Different case) "+count2);
+
+        
+    }
+}
